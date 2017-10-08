c3b9b5637e1aec0f8fc9040a1d33eef04931e7d4
Daniel Fone <daniel@fone.net.nz>
2017-10-08 22:52:29 +1300

    Don't use ApplicationHelper

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,7 @@
 class ContactsController < ApplicationController
 
+  helper_method :external_link_to
+
   def index
     @contacts = Xero::Contact.all.order(:name)
   end
@@ -7,6 +9,14 @@ class ContactsController < ApplicationController
   def sync
     Xero::ContactPullJob.perform_now
     redirect_back fallback_location: :index
+  end
+
+private
+
+  def external_link_to(name = nil, options = nil, html_options={})
+    view_context.link_to options, html_options.merge(target: '_blank') do
+      name
+    end
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
 
-  def external_link_to(name = nil, options = nil, html_options={})
-    link_to options, html_options.merge(target: '_blank') do
-      safe_join [name, " ", '☞']
-    end
-  end
-
 end
