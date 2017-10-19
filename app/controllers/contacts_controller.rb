5bfed012d62f7e44af0c8e023de4962279a7d592
Sarah Hay <sarah.hay@trineo.com>
2017-10-19 21:12:20 +1300

    Rearrange logic in controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,19 +2,37 @@ class ContactsController < ApplicationController
 
   helper_method :external_link_to
 
-  rescue_from Xero::Error, with: :render_xero_error
-
   def index
     @contacts = Xero::Contact.all.order(:name)
   end
 
-  def sync
-    Xero::ContactPullJob.perform_now
-    flash[:success] = "Updated contacts from Xero"
-    redirect_back fallback_location: :index
+  def perform
+    sync!
+    flash_success
+    redirect(:index)
+
+  rescue Xero::Error => error
+    flash_error(error)
+    redirect(:root)
   end
 
 private
+
+  def sync!
+    Xero::ContactPullJob.perform_now
+  end
+
+  def flash_success
+    flash[:success] = "Updated contacts from Xero"
+  end
+
+  def flash_error(error)
+    flash[:error] = "[Xero Error] #{error.message}"
+  end
+
+  def redirect(fallback_location)
+    redirect_back fallback_location: fallback_location
+  end
 
   def external_link_to(name = nil, options = nil, html_options={})
     view_context.link_to options, html_options.merge(target: '_blank') do
@@ -24,10 +42,4 @@ private
       ], ' '
     end
   end
-
-  def render_xero_error(error)
-    flash[:error] = "[Xero Error] #{error.message}"
-    redirect_back fallback_location: :root
-  end
-
 end
