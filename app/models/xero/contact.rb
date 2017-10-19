4db892863e4138a596bc963abd826c7ac967093e
Philip Arndt <git@p.arndt.io>
2017-10-19 20:24:26 +1300

    Use that eigenclass

--- app/models/xero/contact.rb
+++ app/models/xero/contact.rb
@@ -4,34 +4,38 @@ module Xero
       "shortcode=%<shortcode>s&" \
       "redirecturl=%<target_url>s"
 
-    scope :active, -> { where status: 'ACTIVE' }
+    class << self
+      def active
+        where status: 'ACTIVE'
+      end
 
-    def self.upsert_api_response(results, *args, &block)
-      transaction do
-        Array.wrap(results).each do |result|
-          upsert_api_result(result, *args, &block)
+      def upsert_api_response(results, *args, &block)
+        transaction do
+          Array.wrap(results).each do |result|
+            upsert_api_result(result, *args, &block)
+          end
         end
       end
-    end
 
-    def self.upsert_api_result(contact)
-      # https://github.com/jesjos/active_record_upsert
-      upsert(
-        id: contact[:contact_id],
-        name: contact[:name],
-        status: (contact[:contact_status] || 'ACTIVE'),
-        updated_at: contact[:updated_date_utc],
-        synced_at: Time.current,
-        data: contact.to_h,
-      )
-    end
+      def upsert_api_result(contact)
+        # https://github.com/jesjos/active_record_upsert
+        upsert(
+          id: contact[:contact_id],
+          name: contact[:name],
+          status: (contact[:contact_status] || 'ACTIVE'),
+          updated_at: contact[:updated_date_utc],
+          synced_at: Time.current,
+          data: contact.to_h,
+        )
+      end
 
-    def self.last_updated_at
-      maximum(:updated_at)
-    end
+      def last_updated_at
+        maximum(:updated_at)
+      end
 
-    def self.last_synced_at
-      maximum(:synced_at)
+      def last_synced_at
+        maximum(:synced_at)
+      end
     end
 
     # https://developer.xero.com/documentation/api-guides/deep-link-xero
@@ -48,7 +52,9 @@ module Xero
       case value
       when Array
         view_context.content_tag(:ol) do
-          view_context.safe_join value.map { |v| view_context.content_tag(:li, formatted_value(v, view_context)) }
+          view_context.safe_join(value.map { |v|
+            view_context.content_tag(:li, formatted_value(v, view_context))
+          })
         end
       else
         value
