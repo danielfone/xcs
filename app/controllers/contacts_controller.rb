c010b2d4be0f9c81ce9a7bfb881b4a19b761786b
Philip Arndt <git@p.arndt.io>
2017-10-19 21:00:04 +1300

    Betterer if not good

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,5 @@
 class ContactsController < ApplicationController
 
-  helper_method :external_link_to
-
   rescue_from Xero::Error, with: :render_xero_error
 
   def index
@@ -15,15 +13,6 @@ class ContactsController < ApplicationController
   end
 
   private
-
-  def external_link_to(name = nil, options = nil, html_options={})
-    view_context.link_to options, html_options.merge(target: '_blank') do
-      view_context.safe_join [
-        name,
-        view_context.content_tag(:sup, nil, class: "fa fa-external-link", 'aria-hidden' => "true")
-      ], ' '
-    end
-  end
 
   def render_xero_error(error)
     flash[:error] = "[Xero Error] #{error.message}"
