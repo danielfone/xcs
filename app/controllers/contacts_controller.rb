ab3e7b4a962493028da5acc46895c660aa3ee6d2
Philip Arndt <git@p.arndt.io>
2017-10-19 20:21:41 +1300

    all is redundant

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,7 @@ class ContactsController < ApplicationController
   rescue_from Xero::Error, with: :render_xero_error
 
   def index
-    @contacts = Xero::Contact.all.order(:name)
+    @contacts = Xero::Contact.order(:name)
   end
 
   def sync
