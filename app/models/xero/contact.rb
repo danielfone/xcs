c6f075f9effc7c337197be4ef0787560f7b35cb1
Philip Arndt <git@p.arndt.io>
2017-10-19 21:13:05 +1300

    CONSTANT improvements

--- app/models/xero/contact.rb
+++ app/models/xero/contact.rb
@@ -1,5 +1,6 @@
 module Xero
   class Contact < ApplicationRecord
+    CONTACT_VIEW_PATH = "/Contacts/View.aspx?contactID=%<id>s"
     DEEP_LINK_URL = "https://go.xero.com/organisationlogin/default.aspx?" \
       "shortcode=%<shortcode>s&" \
       "redirecturl=%<target_url>s"
@@ -49,8 +50,11 @@ module Xero
 
     # https://developer.xero.com/documentation/api-guides/deep-link-xero
     def xero_url
-      target = "/Contacts/View.aspx?contactID=#{id}"
-      format(DEEP_LINK_URL, shortcode: org_code, target_url: target)
+      format(
+        DEEP_LINK_URL,
+        shortcode: org_code,
+        target_url: format(CONTACT_VIEW_PATH, id: id)
+      )
     end
 
     def muted?
